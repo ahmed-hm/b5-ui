22fa66d7f3f6b7101eb6dcfc2f264a76295269c7
Ahmed Hamdy <hamdimn54@gmail.com>
2022-06-23 10:09:00 +0200

    [Modules] update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from '@app/app.component';
+import { FiltersComponent } from '@components/filters/filters.component';
+import { FooterComponent } from '@components/footer/footer.component';
+import { HeaderComponent } from '@components/header/header.component';
+import { SearchComponent } from '@components/header/search/search.component';
+import { MenuComponent } from '@components/menu/menu.component';
+import { ProductCardComponent } from '@components/products-list/product-card/product-card.component';
+import { ProductsListComponent } from '@components/products-list/products-list.component';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ProductCardComponent,
+    FooterComponent,
+    FiltersComponent,
+    SearchComponent,
+    ProductsListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, StoreModule.forRoot({}, {})],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
