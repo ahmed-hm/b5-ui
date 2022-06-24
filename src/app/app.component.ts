1e8d92d6db1bc57ba6c50279121f00462ce60b88
Ahmed Hamdy <hamdimn54@gmail.com>
2022-06-24 14:23:23 +0200

    [App] updated app component and app module - inialized store module and effects module - loaded categories and first page of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { getCategoriesAction } from './core/state/category/category.actions';
+import { getProductsActions } from './core/state/product/product.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'b5digital-ui-task';
+
+  constructor(private store: Store) {
+    this.store.dispatch(getCategoriesAction());
+    this.store.dispatch(getProductsActions({ skip: 0, limit: 10 }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from '@app/app.component';
 import { FiltersComponent } from '@components/filters/filters.component';
 import { FooterComponent } from '@components/footer/footer.component';
 import { HeaderComponent } from '@components/header/header.component';
@@ -9,7 +9,17 @@ import { SearchComponent } from '@components/header/search/search.component';
 import { MenuComponent } from '@components/menu/menu.component';
 import { ProductCardComponent } from '@components/products-list/product-card/product-card.component';
 import { ProductsListComponent } from '@components/products-list/products-list.component';
+import { categoryEffects } from '@effects/category.effects';
+import { ProductEffects } from '@effects/product.effects';
+import { environment } from '@environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { cartReducer } from '@state/cart/cart.reducers';
+import { categoryReducer } from '@state/category/category.reducers';
+import { productReducer } from '@state/product/product.reducers';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -20,10 +30,21 @@ import { StoreModule } from '@ngrx/store';
     FooterComponent,
     FiltersComponent,
     SearchComponent,
-    ProductsListComponent,
+    ProductsListComponent
   ],
-  imports: [BrowserModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      cart: cartReducer,
+      product: productReducer,
+      category: categoryReducer
+    }),
+    EffectsModule.forRoot([ProductEffects, categoryEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    InfiniteScrollModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
