3df535b74071a1b7154d10648c5ea650ac97af11
Ahmed Hamdy <hamdimn54@gmail.com>
2022-06-24 14:27:31 +0200

    [Header] - add cart counter - update ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { CartTotalProductsSelector } from '@state/cart/cart.selectors';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  cartTotalProducts$ = this.store.select(CartTotalProductsSelector);
+  constructor(private store: Store) {}
 
   ngOnInit(): void {}
 }
