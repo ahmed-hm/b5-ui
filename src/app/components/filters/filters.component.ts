a4c338cd5dd8c5c85ff689df7e5df4b98faed60a
Ahmed Hamdy <hamdimn54@gmail.com>
2022-06-24 14:25:03 +0200

    [Filters] updated filters component - add filters functionality - render loaded categories

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '@interfaces/category.interface';
+import { Store } from '@ngrx/store';
+import { categoriesSelector } from '@state/category/category.selectors';
+import { getProductsActions, setSelectedCategoryActions } from '@state/product/product.actions';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.scss'],
+  styleUrls: ['./filters.component.scss']
 })
 export class FiltersComponent implements OnInit {
-  constructor() {}
+  categories$ = this.store.select(categoriesSelector);
+
+  constructor(private store: Store) {}
 
   ngOnInit(): void {}
+
+  filterByCategory(category: Category) {
+    this.store.dispatch(setSelectedCategoryActions({ category }));
+    this.store.dispatch(getProductsActions({ limit: 10, skip: 0 }));
+  }
 }
